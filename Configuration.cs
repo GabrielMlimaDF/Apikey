0961dad8d48922747e06f40ad44a1c97d0fdf896
GabrielMlimaDF <gabrieldevbrasilia@gmail.com>
2025-03-06 19:54:43 -0300

    Add Configuration / appsettings

--- Configuration.cs
+++ Configuration.cs
@@ -2,7 +2,7 @@
 {
     public static class Configuration
     {
-        public static string ApiKeyName = "api_key";
-        public static string ApiKey = "e6cf4786-c9b5-49f0-9521-bfbeef74b50c";
+        public static string? ApiKeyName;
+        public static string? ApiKey;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using ApiKey;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,9 @@ builder.Services.AddControllers();
 var connectionString = builder.Configuration.GetConnectionString("Banco");
 builder.Services.AddDbContext<ContextApp>(op=>op.UseSqlServer(connectionString));
 var app = builder.Build();
+Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
+Configuration.ApiKey = app.Configuration.GetValue<string>("ApiKey");
+
 
 app.MapControllers();
 
